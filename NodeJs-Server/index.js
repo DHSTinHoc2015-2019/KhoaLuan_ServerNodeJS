4e81595770a27aeab75fc433147b6f1e15aabdbf
tranquangtanqt <tranquangtanqt1990@gmail.com>
2019-02-12 18:04:52 +0700

    Discussion_Admin

--- NodeJs-Server/index.js
+++ NodeJs-Server/index.js
@@ -34,21 +34,50 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/demoLike', function (req, res) {
-	con.connect(function(err) {
-		con.query("SELECT * FROM demolike", function (err, result, fields) {
-			res.send(result)
-			// console.log(result);
-		});
-	});
-});
+// app.get('/demoLike', function (req, res) {
+//  con.connect(function(err) {
+//      con.query("SELECT * FROM demolike", function (err, result, fields) {
+//          res.send(result)
+//          // console.log(result);
+//      });
+//  });
+// });
 
 io.on('connection', function (socket) {
-	console.log(`Đã có thằng vào với id là: ${socket.id}`);
-	// socket.emit('news', { hello: 'world' });
-	socket.on('ten_tin_hieu', function (data) {
-		console.log(data)
-		// socket.emit('tin_hieu_ve')
-		io.emit('tin_hieu_ve', data)
-	});
+    console.log(`Đã có thằng vào với id là: ${socket.id}`);
+    // socket.emit('news', { hello: 'world' });
+    socket.on('ten_tin_hieu', function (data) {
+        console.log(data)
+        // socket.emit('tin_hieu_ve')
+        io.emit('tin_hieu_ve', data)
+    });
+
+    socket.on('create_comment', function (data) {
+        console.log(data)
+        io.emit('create_comment', data)
+    });
+    socket.on('delete_comment', function (data) {
+        console.log(data)
+        io.emit('delete_comment', data)
+    });
+    socket.on('edit_comment', function (data) {
+        console.log(data)
+        io.emit('edit_comment', data)
+    });
+    socket.on('like_discussion', function (data) {
+        console.log(data)
+        io.emit('like_discussion', data)
+    }); 
+    socket.on('create_comment_detail', function (data) {
+        console.log(data)
+        io.emit('create_comment_detail', data)
+    });
+    socket.on('edit_comment_detail', function (data) {
+        console.log(data)
+        io.emit('edit_comment_detail', data)
+    });
+    socket.on('delete_comment_detail', function (data) {
+        console.log(data)
+        io.emit('delete_comment_detail', data)
+    });
 });
