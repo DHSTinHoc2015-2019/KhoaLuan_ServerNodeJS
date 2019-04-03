83dee8e1111461f1ded4b3946f6d8eb497cc99b5
tranquangtanqt <tranquangtanqt1990@gmail.com>
2019-04-03 11:15:02 +0700

    blog

--- NodeJs-Server/index.js
+++ NodeJs-Server/index.js
@@ -35,23 +35,24 @@ app.use(function (req, res, next) {
 });
 
 // app.get('/demoLike', function (req, res) {
-//  con.connect(function(err) {
-//      con.query("SELECT * FROM demolike", function (err, result, fields) {
-//          res.send(result)
-//          // console.log(result);
-//      });
-//  });
+// 	con.connect(function(err) {
+// 		con.query("SELECT * FROM demolike", function (err, result, fields) {
+// 			res.send(result)
+// 			// console.log(result);
+// 		});
+// 	});
 // });
 
 io.on('connection', function (socket) {
-    console.log(`Đã có thằng vào với id là: ${socket.id}`);
-    // socket.emit('news', { hello: 'world' });
-    socket.on('ten_tin_hieu', function (data) {
-        console.log(data)
-        // socket.emit('tin_hieu_ve')
-        io.emit('tin_hieu_ve', data)
-    });
+	console.log(`Đã có tín hiệu vào với id là: ${socket.id}`);
+	// socket.emit('news', { hello: 'world' });
+	socket.on('ten_tin_hieu', function (data) {
+		console.log(data)
+		// socket.emit('tin_hieu_ve')
+		io.emit('tin_hieu_ve', data)
+	});
 
+    //Bình luận thảo luận
     socket.on('create_comment', function (data) {
         console.log(data)
         io.emit('create_comment', data)
@@ -80,4 +81,40 @@ io.on('connection', function (socket) {
         console.log(data)
         io.emit('delete_comment_detail', data)
     });
+
+
+
+    //Bình luận blog
+    socket.on('blog_create_comment', function (data) {
+        console.log(data)
+        io.emit('blog_create_comment', data)
+    });
+
+    socket.on('blog_edit_comment', function (data) {
+        console.log(data)
+        io.emit('blog_edit_comment', data)
+    });
+
+    socket.on('blog_delete_comment', function (data) {
+        console.log(data)
+        io.emit('blog_delete_comment', data)
+    });
+
+    socket.on('blog_create_comment_detail', function (data) {
+        console.log(data)
+        io.emit('blog_create_comment_detail', data)
+    });
+    socket.on('blog_edit_comment_detail', function (data) {
+        console.log(data)
+        io.emit('blog_edit_comment_detail', data)
+    });
+    socket.on('blog_delete_comment_detail', function (data) {
+        console.log(data)
+        io.emit('blog_delete_comment_detail', data)
+    });
+
+    socket.on('like_blog', function (data) {
+        console.log(data)
+        io.emit('like_blog', data)
+    }); 
 });
